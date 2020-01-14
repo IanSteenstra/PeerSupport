b1b17fa6cd1c77418a6e98d20f54d4badf277530
IanSteenstra <steenstra.ian@gmail.com>
2020-01-13 23:35:04 -0500

    React & Django REST

---
+++ backend/src/chat/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from chat.models import Chat, Contact
+from chat.views import get_user_contact
+
+
+class ContactSerializer(serializers.StringRelatedField):
+    def to_internal_value(self, value):
+        return value
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    participants = ContactSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = ('id', 'messages', 'participants')
+        read_only = ('id')
+
+    def create(self, validated_data):
+        print(validated_data)
+        participants = validated_data.pop('participants')
+        chat = Chat()
+        chat.save()
+        for username in participants:
+            contact = get_user_contact(username)
+            chat.participants.add(contact)
+        chat.save()
+        return chat
---
+++ frontend/gui/src/containers/Layout.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
+
+const { Header, Content, Footer } = Layout;
+
+class CustomLayout extends React.Component {
+    render() {
+        return (
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">
+                            <Link to="/">Home</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="/profile">Profile</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Link to="/chat">Chat</Link>
+                        </Menu.Item>
+                        {
+                            this.props.isAuthenticated ?
+
+                                <Menu.Item key="4" onClick={this.props.logout}>
+                                    Logout
+                                </Menu.Item>
+
+                                :
+
+                                <Menu.Item key="4">
+                                    <Link to="/login/">Login</Link>
+                                </Menu.Item>
+                        }
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                        {this.props.children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.logout())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CustomLayout);
