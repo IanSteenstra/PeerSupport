547f83aa988cd92deeb94753f03eb911a86bb680
Andrew Gaudet <amg0508@gmail.com>
2020-02-28 16:47:52 -0500

    Got research quiz to show up in django admin backend view

--- backend/src/Profile/admin.py
+++ backend/src/Profile/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Profile, UserQuiz, CounselorQuiz
+from .models import Profile, UserQuiz, CounselorQuiz, ResearchQuiz
 
 admin.site.register(Profile)
 admin.site.register(UserQuiz)
 admin.site.register(CounselorQuiz)
+admin.site.register(ResearchQuiz)
