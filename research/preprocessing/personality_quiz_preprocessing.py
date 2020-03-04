1e6683c1cc1b970a49dfe3ff9acc35b1fbec0fbb
IanSteenstra <steenstra.ian@gmail.com>
2020-03-03 20:42:59 -0500

    Preprocessed personality quiz data for training

---
+++ research/preprocessing/personality_quiz_preprocessing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("personality_quiz_responses.csv")
+
+cat_columns = df.select_dtypes(['object']).columns[1:]
+for col in cat_columns:
+    df[col] = df[col].astype('category')
+
+df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
+
+df.to_csv(r'C:\Users\steeni\Documents\Github\TherapyNow\research\training\processed_personality_quiz_responses.csv')
