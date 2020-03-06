0883211e32acc3e9fef9d96f0d23dfa8b1943865
chris1889 <33167580+chris1889@users.noreply.github.com>
2020-03-06 12:49:36 -0500

    Update routes.js

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -4,23 +4,15 @@ import { Route } from 'react-router-dom';
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import Profile from "./containers/Profile";
-<<<<<<< HEAD
-import Home from "./containers/Home";
-import Quiz from "./containers/Quiz";
-=======
 import Quiz from "./containers/Quiz";
 import Home from "./containers/Home";
 import ChatLayout from "./containers/Chat";
->>>>>>> chat_page
 
 const BaseRouter = () => (
     <div>
         <Route exact path="/login/" component={Login} />
         <Route exact path="/signup/" component={Signup} />
-<<<<<<< HEAD
-=======
         <Route exact path="/chat/" component={ChatLayout} />
->>>>>>> chat_page
         <Route exact path="/profile/" component={Profile} />
         <Route exact path="/quiz/" component={Quiz} />
         <Route exact path="/" component={Home} />
