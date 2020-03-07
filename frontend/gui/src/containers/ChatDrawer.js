0477000b9e43738bb0c98a9e656a8e79c3298713
UnitedBagels <unitedbagels@gmail.com>
2020-03-07 16:09:12 -0500

    Added basic frontend chat UI
    Search box does not clear

--- frontend/gui/src/containers/ChatDrawer.js
+++ frontend/gui/src/containers/ChatDrawer.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { Tabs, Radio, Drawer, Input, Button } from 'antd';
+import { Tabs, Radio, Drawer, Input, Button, List } from 'antd';
 import { Row, Col } from 'antd';
+import MessageInstance from "./MessageInstance";
 import * as actions from "../store/actions/userData";
 
 import { connect } from "react-redux";
 import { setUsername } from "../store/actions/userData";
 import Text from "antd/es/typography/Text";
 const { TabPane } = Tabs;
+const { Search } = Input;
+
+const data = [
+    'Test1',
+    'Test2'
+];
+
 class ChatDrawer extends React.Component {
 
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         username: "nullUser"
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: data
+        };
+    }
+
+    // Function that handles event of sending a message
+    // Currently, the only data that is sent is the message itself
+    // and the username of who it is being sent to.
+    onSend(value, usr) {
+        var newStateArray = [...this.state.data];
+        newStateArray.push("Default User: " + value);
+        //document.getElementById("message-box").value="";
+        this.setState(() => {
+            return {
+                data: newStateArray
+            };
+        });
+    };
 
     render() {
-        //this.props.setUsername("yeetles")
         return (
             <div>
                 <Drawer
@@ -28,20 +49,26 @@ class ChatDrawer extends React.Component {
                     visible={this.props.visible}
                     getContainer={false}
                     width="60%"
-                    style={{ position: 'absolute' }}>
+                    height ="100%"
+                    >
+                    <List
+                        id="msg-list"
+                        dataSource={this.state.data}
+                        renderItem={item => <List.Item>{item}</List.Item>}>
+                    </List>
                     <div style={{position: 'absolute',
-                                bottom: 0,
-                                width: 430,
-                                padding: 20}}>
+                        bottom: 0,
+                        width: "90%",
+                        padding: 20}}>
                         <Row>
                             <Col span={18}>
-                                <Input placeholder="Enter a message...">
-                                </Input>
-                            </Col>
-                            <Col span={5} offset={1}>
-                                <Button type="primary">
-                                    Send
-                                </Button>
+                                <Search
+                                    id="message-box"
+                                    placeholder="Enter a message..."
+                                    enterButton="Send"
+                                    onSearch={value =>
+                                        this.onSend(value, this.props.username)}>
+                                </Search>
                             </Col>
                         </Row>
                     </div>
