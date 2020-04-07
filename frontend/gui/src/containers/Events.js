1a4bcf164a6b3bcf000b0018084a4e530174308f
BrandonHerrada <brandonmherrada@gmail.com>
2020-04-07 13:32:14 -0700

    Added Join Buttons to Events Table

--- frontend/gui/src/containers/Events.js
+++ frontend/gui/src/containers/Events.js
@@ -3,84 +3,95 @@ import { Table } from 'antd';
 import Item from 'antd/lib/list/Item';
 import Button from 'react-bootstrap/Button';
 
-const columns = [
-    {
-        title: 'Event',
-        dataIndex: 'name',
-        key: 'name',
-        render: text => <a>{text}</a>
-    },
-    {
-        title: 'Start Time',
-        dataIndex: 'start_time',
-        key: 'start_time',
-    },
-    {
-        title: 'End Time',
-        dataIndex: 'end_time',
-        key: 'end_time',
-    },
-    {
-        title: 'Description',
-        dataIndex: 'description',
-        key: 'description',
-    },
-    {
-        title: 'Status',
-        key: 'status',
-    },
-];
 
-const events = [
-    {
-        key: '1',
-        name: 'Active Minds',
-        start_time: 'March 12th, 2020',
-        end_time: 'March 12th, 2020',
-        description: 'words',
-        status: false,
-    },
-    {
-        key: '2',
-        name: 'Mindfulness Week',
-        start_time: 'April 6th, 2020',
-        end_time: 'April 10th, 2020',
-        description: 'stuff',
-        status: true,
-    },
-    {
-        key: '3',
-        name: 'Finals',
-        start_time: 'May 1st, 2020',
-        end_time: 'May 5th, 2020',
-        description: 'stress',
-        status: false,
-    },
-    {
-        key: '4',
-        name: 'Online Graduation',
-        start_time: 'May 23th, 2020',
-        end_time: 'March 23th, 2020',
-        description: 'Graduation',
-        status: true,
-    },
-];
-
-export class Events extends React.Component {
+class Events extends React.Component {
     constructor(props) {
         super(props);
+        this.columns = [
+            {
+                title: 'Event',
+                dataIndex: 'name',
+                key: 'name',
+                render: text => <a>{text}</a>
+            },
+            {
+                title: 'Start Time',
+                dataIndex: 'start_time',
+                key: 'start_time',
+            },
+            {
+                title: 'End Time',
+                dataIndex: 'end_time',
+                key: 'end_time',
+            },
+            {
+                title: 'Description',
+                dataIndex: 'description',
+                key: 'description',
+            },
+            {
+                title: 'Action',
+                dataIndex: '',
+                key: 'x',
+                render: (text, record) => (
+                    <span>
+                        {/* 
+                        TODO - Add functionality to disable button after time expires
+                        */}
+                        <Button variant="success">Join</Button>
+                    </span>
+                ),
+            },
+        ];
+
+        this.events = [
+            {
+                key: '1',
+                name: 'Active Minds',
+                start_time: 'March 12th, 2020',
+                end_time: 'March 12th, 2020',
+                description: 'words',
+                status: false,
+            },
+            {
+                key: '2',
+                name: 'Mindfulness Week',
+                start_time: 'April 6th, 2020',
+                end_time: 'April 10th, 2020',
+                description: 'stuff',
+                status: true,
+            },
+            {
+                key: '3',
+                name: 'Finals',
+                start_time: 'May 1st, 2020',
+                end_time: 'May 5th, 2020',
+                description: 'stress',
+                status: false,
+            },
+            {
+                key: '4',
+                name: 'Online Graduation',
+                start_time: 'May 23th, 2020',
+                end_time: 'March 23th, 2020',
+                description: 'Graduation',
+                status: true,
+            },
+        ];
         this.state = {
-            columns: columns,
-            eventsList: events,
+            columns: this.columns,
+            eventsList: this.events,
             viewStatus: false
         };
     }
+
     displayStatus = status => {
         if (status) {
             return this.setState({ status: true });
         }
         return this.setState({ status: false });
     };
+
     renderTabList = () => {
         return (
             <div className="my-5 tab-list">
@@ -99,58 +110,17 @@ export class Events extends React.Component {
             </div>
         );
     };
-    renderEvents = () => {
-        const { viewStatus } = this.state;
-        const newEvents = this.state.eventsList.filter(
-            event => event.status === viewStatus
-        );
-        return newEvents.map(event => (
-            <li
-                key={event.key}
-                className="list-group-item d-flex justify-content-between align-items-center"
-            >
-                <span
-                    className={`event-name mr-2 ${
-                        this.state.viewStatus ? "ended-event" : ""
-                        }`}
-                    title={event.description}
-                >
-                </span>
-                <div className="col-md-2 mx-auto p-0">{event.name}</div>
-                <div className="col-md-4 mx-auto p-0">{event.start_time}</div>
-                <div className="col-md-4 mx-auto p-0">{event.end_time}</div>
-                <span>
-                    {/* 
-                        TODO - Add functionality to disable button after time expires
-                    */}
-                    <Button variant="success">Join</Button>{' '}
-                </span>
-            </li>
-        ));
-    }
+
+
     render() {
         return (
-            // <main className="content">
-            //     {/* <h1 className="text-white text-uppercase text-center my-4">Events app</h1> */}
-            //     <div className="row ">
-            //         <div className="col-md-10 mx-auto p-0">
-            //             {this.renderTabList()}
-            //             <div><Table columns={columns} events={events} /></div>
-            //             {/* <ul className="list-group list-group-flush">
-            //                 {this.renderEvents()}
-            //             </ul> */}
-            //         </div>
-            //     </div>
-            // </main>
             <div>
-                <Table columns={columns} dataSource={events} />
-            </div>
+                <Table
+                    columns={this.state.columns}
+                    dataSource={this.state.eventsList}
+                />
+            </div >
         );
     }
 }
-// return (
-//     <div>
-//         <Table columns={columns} dataSource={data} />
-//     </div>
-// )
 export default Events;
