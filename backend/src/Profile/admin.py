11a5f336765bb5af3d8799c77007aa07f47839d9
BrandonHerrada <brandonmherrada@gmail.com>
2020-04-17 14:51:36 -0700

    Added UserQuizzes, Counselor Quizzes and Research Quizzes back

--- backend/src/Profile/admin.py
+++ backend/src/Profile/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from .models import Profile, UserQuiz, CounselorQuiz, ResearchQuiz, Counselor
 
-from .models import Profile
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'alias')
 
-admin.site.register(Profile)
+class UserQuizAdmin(admin.ModelAdmin):
+    list_display = ('profile', 
+                    'happiness', 
+                    'hometown', 
+                    'receiving_giving', 
+                    'religion', 
+                    'relation_to_others', 
+                    'government_assistance', 
+                    'local_community', 
+                    'immediate_family', 
+                    'fulfilling_work', 
+                    'leader_social_circle', 
+                    'created')
+
+class CounselorQuizAdmin(admin.ModelAdmin):
+	list_display = ('profile', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'created')
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserQuiz, UserQuizAdmin)
+admin.site.register(CounselorQuiz, CounselorQuizAdmin)
+admin.site.register(ResearchQuiz)
